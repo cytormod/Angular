0123ba2cd7fc66e61820224f06c0862068c2b92c
cytormod <sahil.gopani5@gmail.com>
2025-09-13 20:08:56 +0200

    =Add userComponents

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
