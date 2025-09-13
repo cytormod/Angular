5bb522fc3ec3720f058a84624e2d1e4e47d8b8eb
cytormod <sahil.gopani5@gmail.com>
2025-09-14 00:40:04 +0200

    =Add Getter instead of String inerpolation and Property Binding Syntax

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -13,6 +13,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   // We will add some code in the body of the class
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+  get user_Name() {
+    return this.selectedUser.name
+  }
 }
 //What's the advantage of writing the code inside the class?
 // We can now access the "selecteduser" Property inside the template of the templateUrl. So all the properties that we are defining in the component class are available in the template of that component
+
+// What is Getter?
+// It's essentially a method, so a function inside the class, that's usable like property so that does not need to be called and executed explicitly, and that's meant to produce and return a new value. Here the new value is that Path image.
