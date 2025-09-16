860f33de254efd9e63bc153d253635f08308a34f
cytormod <sahil.gopani5@gmail.com>
2025-09-16 09:15:00 +0200

    =Add Signal method 😕

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,13 +10,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
+// one way of doing (updating the state of Application)
+
+// 2nd Way: Signals: It is the special kind of values that tell Angular when they change.
+
+// The idea behind the signal is that you don't just have some initial values in them, but that you can also change those values.
+// You can change the value of the signal by calling the set method on that signal Object.
 export class UserComponent {
   // We will add some code in the body of the class
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
-  }
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar
+  // }
   get user_Name() {
     return this.selectedUser.name
   }
@@ -24,7 +33,7 @@ export class UserComponent {
   onSelectUser() {
     console.log('clicked');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
     
   }
 }
