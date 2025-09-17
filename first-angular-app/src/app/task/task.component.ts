26cfdb1467826bba6b0fa0990f7f60b094c00d51
cytormod <sahil.gopani5@gmail.com>
2025-09-17 09:48:18 +0200

    =TS Work with Potentially Undefined Values and union types. + . Fix Bugs of name

--- first-angular-app/src/app/task/task.component.ts
+++ first-angular-app/src/app/task/task.component.ts
@@ -9,5 +9,7 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) name!: string;
+  // We are giving as a option because, there might be chance, that one might be able to find the existed userId.
+  @Input() name?: string;
 }
