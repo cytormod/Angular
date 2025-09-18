f0a13b2008c0796abf724db47c221e3b554a7d64
cytormod <sahil.gopani5@gmail.com>
2025-09-18 15:41:31 +0200

    =Output User-Specific Task, not the actual data

--- first-angular-app/src/app/tasks/task.component.ts
+++ first-angular-app/src/app/tasks/task.component.ts
@@ -12,5 +12,34 @@ import { Task } from './task/task.component';
 export class TaskComponent {
   // @Input({required: true}) name!: string;
   // We are giving as a option because, there might be chance, that one might be able to find the existed userId.
-  @Input() name?: string;
+  @Input({required: true}) userId!:string;
+  @Input({required: true}) name?: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advance Features of Angular & how to apply them',
+      dueDate: '2025-12-31'
+    },
+    {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary:
+      'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+  },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId)
+  }
 }
